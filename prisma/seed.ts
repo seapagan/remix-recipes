f94d4f1d692464fed9bf1b2ad5a15bbf4cd0fe3d
Grant Ramsay <seapagan@gmail.com>
2023-11-25 21:07:29 +0000

    seed the database

---
+++ prisma/seed.ts
@@ -0,0 +1,45 @@
+import { PrismaClient } from "@prisma/client";
+
+const db = new PrismaClient();
+
+const getShelves = () => {
+  return [
+    {
+      name: "Dairy",
+      items: {
+        create: [
+          {
+            name: "Milk",
+          },
+          {
+            name: "Eggs",
+          },
+          {
+            name: "Cheese",
+          },
+        ],
+      },
+    },
+    {
+      name: "Fruits",
+      items: {
+        create: [
+          {
+            name: "Apples",
+          },
+          {
+            name: "Oranges",
+          },
+        ],
+      },
+    },
+  ];
+};
+
+const seed = async () => {
+  await Promise.all(
+    getShelves().map((shelf) => db.pantryShelf.create({ data: shelf }))
+  );
+};
+
+seed();
