605288b8b689ae07b26273cdfe8cce87ebad98e3
Grant Ramsay <seapagan@gmail.com>
2023-11-26 10:52:42 +0000

    start adding search form

--- app/routes/app/pantry.tsx
+++ app/routes/app/pantry.tsx
@@ -1,7 +1,68 @@
-export default function Pantry() {
+import { type LoaderFunctionArgs, json } from "@remix-run/node";
+import { useLoaderData, useSearchParams } from "@remix-run/react";
+import classNames from "classnames";
+import { SearchIcon } from "~/components/icons";
+import { getAllShelves } from "~/models/pantry-shelf.server";
+
+export const loader = async ({ request }: LoaderFunctionArgs) => {
+  const url = new URL(request.url);
+  const q = url.searchParams.get("q");
+  const shelves = await getAllShelves(q);
+  return json({ shelves });
+};
+export const Pantry = () => {
+  const data = useLoaderData<typeof loader>();
+  const [searchParams] = useSearchParams();
+
   return (
     <div>
-      <h1>Welcome to the pantry :)</h1>
+      <form
+        className={classNames(
+          "flex border-2 borger-gray-300 rounded-md",
+          "focus-within:border-primary md:w-80"
+        )}
+      >
+        <button className="px-2 mr-1">
+          <SearchIcon />
+        </button>
+        <input
+          defaultValue={searchParams.get("q") ?? ""}
+          type="text"
+          name="q"
+          autoComplete="off"
+          placeholder="Search Shelves..."
+          id=""
+          className="w-full py-3 px-2 outline-none"
+        />
+      </form>
+      <ul
+        className={classNames(
+          "flex gap-8 overflow-x-auto mt-4",
+          "snap-x snap-mandatory md:snap-none"
+        )}
+      >
+        {data.shelves.map((shelf) => (
+          <li
+            key={shelf.id}
+            className={classNames(
+              "border-2 border-primary rounded-md p-4",
+              "w-[calc(100vw-2rem)] flex-none snap-center h-fit",
+              "md:w-96"
+            )}
+          >
+            <h1 className="text-2xl font-extrabold mb-2">{shelf.name}</h1>
+            <ul>
+              {shelf.items.map((item) => (
+                <li key={item.id} className="py-2">
+                  {item.name}
+                </li>
+              ))}
+            </ul>
+          </li>
+        ))}
+      </ul>
     </div>
   );
-}
+};
+
+export default Pantry;
